64711c51a76e35148da6f903ba5b40d000eda986
unknown <elik@example.com>
2023-12-08 14:30:01 +0300

    name of commit

---
+++ final-project/src/pages/footer/footer.js
@@ -0,0 +1,13 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <span className='footer-text'>+7904(531)0976</span>
+            <span className='footer-text'>lika-moon@list.ru</span>
+            <span className='footer-text'>г. Белгород, Проезд Фреймворщиков д.9</span>
+        </div>
+    )
+}
+
+export default Footer;
